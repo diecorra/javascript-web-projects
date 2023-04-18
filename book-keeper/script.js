2d044762fb038c43e1efff750d034e358144c4ea
Diego Corradi <diego.corradi95@gmail.com>
2023-04-18 17:06:24 +0200

    work on localStorage functionality

--- book-keeper/script.js
+++ book-keeper/script.js
@@ -1,22 +1,24 @@
-const modal = document.getElementById("modal");
-const modalShow = document.getElementById("show-modal");
-const modalClose = document.getElementById("close-modal");
-const bookmarkForm = document.getElementById("bookmark-form");
-const websiteNameEl = document.getElementById("website-name");
-const websiteUrlEl = document.getElementById("website-url");
-const bookmarkContainer = document.getElementById("bookmarks-container");
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarkContainer = document.getElementById('bookmarks-container');
+
+let bookmarks = [];
 
 function showModal() {
-  modal.classList.add("show-modal");
+  modal.classList.add('show-modal');
   websiteNameEl.focus();
 }
 
-modalShow.addEventListener("click", showModal);
-modalClose.addEventListener("click", () =>
-  modal.classList.remove("show-modal")
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () =>
+  modal.classList.remove('show-modal')
 );
-window.addEventListener("click", (e) =>
-  e.target === modal ? modal.classList.remove("show-modal") : false
+window.addEventListener('click', (e) =>
+  e.target === modal ? modal.classList.remove('show-modal') : false
 );
 
 function validate(nameValue, urlValue) {
@@ -24,27 +26,91 @@ function validate(nameValue, urlValue) {
     /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
   const regex = new RegExp(expression);
   if (!nameValue || !urlValue) {
-    alert("Please submit values!");
+    alert('Please submit values!');
     return false;
   }
   if (!urlValue.match(regex)) {
-    alert("Please provide a valid address!");
+    alert('Please provide a valid address!');
     return false;
   }
   // Valid
   return true;
 }
 
+// Build Boomarks DOM
+function buildBookmarks() {
+  // Build items
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    // item
+    const item = document.createElement('div');
+    item.classList.add('item');
+    // Close icon
+    const closeIcon = document.createElement('i');
+    closeIcon.classList.add('fas', 'fa-times');
+    closeIcon.setAttribute('title', 'Delete Bookmark');
+    closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+    // Favicon / Link Container
+    const linkInfo = document.createElement('div');
+    linkInfo.classList.add('name');
+    // Favicon
+    const favicon = document.createElement('img');
+    favicon.setAttribute(
+      'src',
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute('alt', 'Favicon');
+    // Link
+    const link = document.createElement('a');
+    link.setAttribute('href', `${url}`);
+    link.setAttribute('target', '_blank');
+    link.textContent = name;
+    // Append to boomarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarkContainer.appendChild(item);
+  });
+}
+
+// Fetch Bookmarks
+function fetchBookmarks() {
+  // Get bookmarks from localStorage if available
+  if (localStorage.getItem('bookmarks')) {
+    bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  } else {
+    // Create bookmarks array in localStorage
+    bookmarks = [
+      {
+        name: 'Diego',
+        url: 'https://diegocorradi.netlify.app/',
+      },
+    ];
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+  buildBookmarks();
+}
+
 function storeBookmark(e) {
   e.preventDefault();
   const nameValue = websiteNameEl.value;
   let urlValue = websiteUrlEl.value;
-  if (!urlValue.includes("https://") && !urlValue.includes("http://")) {
+  if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
     urlValue = `https://${urlValue}`;
   }
   if (!validate(nameValue, urlValue)) {
     return false;
   }
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
 }
 
-bookmarkForm.addEventListener("submit", storeBookmark);
+bookmarkForm.addEventListener('submit', storeBookmark);
+
+fetchBookmarks();
