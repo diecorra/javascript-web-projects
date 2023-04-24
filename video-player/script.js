ffd2f7aeadf2250d7ec1acaf1d6a16020e1b47d1
diecorra <diego.corradi95@gmail.com>
2023-04-24 18:49:52 +0200

    first commit video-player project

---
+++ video-player/script.js
@@ -0,0 +1,66 @@
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.querySelector('#play-btn');
+const volumeIcon = document.querySelector('#volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
+
+// Play & Pause ----------------------------------- //
+
+function showPlayIcon() {
+  playBtn.setAttribute('title', 'Play');
+  playBtn.classList.replace('fa-pause', 'fa-play');
+}
+
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+  } else {
+    video.pause();
+    showPlayIcon();
+  }
+}
+
+// On Video End
+video.addEventListener('ended', showPlayIcon);
+
+// Progress Bar ---------------------------------- //
+
+// Calculate display time format
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+}
+
+// Update progress bar
+function updateProgress() {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
+}
+
+// Click to seek within the video
+function setProgress(e) {
+  console.log(e);
+}
+
+// Volume Controls --------------------------- //
+
+// Change Playback Speed -------------------- //
+
+// Fullscreen ------------------------------- //
+
+// Event Listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
